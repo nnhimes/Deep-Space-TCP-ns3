ea2e0715d03d79d0a21a0926d475b545ef1d2826
Nicholas Himes <41524135+nnhimes@users.noreply.github.com>
2021-03-29 19:11:26 -0400

    Create average throughput tracker that outputs at end

--- tcpCompare.cc
+++ tcpCompare.cc
@@ -18,6 +18,7 @@ NS_LOG_COMPONENT_DEFINE ("tcpCompare");
 // Global Variables
 Time mostRecentSimTime;
 int simulationMaxTime = 500;
+Ptr<PacketSink> sink; //Used for average throughput calculations
 
 // MyApp Definition
 class MyApp : public Application 
@@ -177,7 +178,7 @@ main (int argc, char *argv[])
   devices = pointToPoint.Install (nodes);
 
   Ptr<RateErrorModel> em = CreateObject<RateErrorModel> ();
-  em->SetAttribute ("ErrorRate", DoubleValue (0.00001));
+  em->SetAttribute ("ErrorRate", DoubleValue (0.0005));
   devices.Get (1)->SetAttribute ("ReceiveErrorModel", PointerValue (em));
 
   InternetStackHelper stack;
@@ -191,6 +192,7 @@ main (int argc, char *argv[])
   Address sinkAddress (InetSocketAddress (interfaces.GetAddress (1), sinkPort));
   PacketSinkHelper packetSinkHelper ("ns3::TcpSocketFactory", InetSocketAddress (Ipv4Address::GetAny (), sinkPort));
   ApplicationContainer sinkApps = packetSinkHelper.Install (nodes.Get (1));
+  sink = StaticCast<PacketSink> (sinkApps.Get (0));
   sinkApps.Start (Seconds (0.));
   sinkApps.Stop (Seconds (simulationMaxTime)); //Global variable defined at top
 
@@ -198,7 +200,7 @@ main (int argc, char *argv[])
   ns3TcpSocket->TraceConnectWithoutContext ("CongestionWindow", MakeCallback (&CwndChange));
 
   Ptr<MyApp> app = CreateObject<MyApp> ();
-  app->Setup (ns3TcpSocket, sinkAddress, 1040, 1000, DataRate ("1Mbps")); //Setup to send 1000 packets of size 1040 with a rate of 1Mbps. Total size: 1040000 bytes
+  app->Setup (ns3TcpSocket, sinkAddress, 1040, 1000, DataRate ("1Mbps")); //Setup to send 1000 packets of size 1040 bytes with a rate of 1Mbps. Total size: 1040000 bytes
   // 1040000 bytes == 8,320,000 bits. This / 1,000,000 bits per second = 8.32 seconds. This is what the simulation's total time is, so it works!
   nodes.Get (0)->AddApplication (app);
   app->SetStartTime (Seconds (1.)); //Must remain at 1 second so there is time to start up application and sockets
@@ -211,7 +213,10 @@ main (int argc, char *argv[])
   Simulator::Run ();
   Simulator::Destroy ();
 
-  std::cout << "\nTotal simulation time (seconds): " << (mostRecentSimTime - startTime).GetSeconds () << "\n";
+  Time totalSimTime = mostRecentSimTime - startTime;
+  std::cout << "\nTotal simulation time (seconds): " << totalSimTime.GetSeconds () << "\n";
+  double averageThroughput = ((sink->GetTotalRx () * 8) / (1e6 * totalSimTime.GetSeconds ()));
+  std::cout << "\nAverage throughput: " << averageThroughput << " Mbit/s" << std::endl;
   return 0;
 }
 
